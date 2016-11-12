35646ddf34b124a61f3ed04c96b8f4d4708bd619
Anton Relin <arelin@Antons-MacBook.local>
2016-11-12 14:55:27 -0500

    tweepy fiddling

--- twitterbot.py
+++ twitterbot.py
@@ -5,3 +5,8 @@ auth.set_access_token("797478447283077121-LsPU9sTIlxD7Mp54hYtoH1N9mR86rUV", "HAI
 
 api = tweepy.API(auth)
 
+statuses = tweepy.Cursor(api.user_timeline, max_id="796897928048766976", user_id="25073877", count="200").items()
+
+for s in statuses:
+    if("obama" in s.text.lower()):
+        print(s.text)
