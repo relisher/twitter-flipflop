c0675c0d8e877774a812e452c06f95ebfb358e23
Anton Relin <arelin@sas.upenn.edu>
2016-11-12 19:38:35 -0500

    methodical twitterbot

--- twitterbot.py
+++ twitterbot.py
@@ -1,13 +1,5 @@
 import tweepy
 
-auth = tweepy.OAuthHandler("xmE8yTuZ65lFReEbDUEIn5aqV", "z0tlMMPm7fxmIM5o21cbhfG5SS0NPgBLF6w4YxhyR0kCi4eeyE")
-auth.set_access_token("797478447283077121-LsPU9sTIlxD7Mp54hYtoH1N9mR86rUV", "HAI9RdL4Mci6c2V5fAt4eK0wO3Geyat1Oq7KAxDINTKlu")
-
-api = tweepy.API(auth)
-
-statusesTrump = tweepy.Cursor(api.user_timeline, user_id="25073877", count="1000").items()
-statusesClinton = tweepy.Cursor(api.user_timeline, user_id="1339835893", count="1000").items;
-
 trumpObama =[];
 trumpRigged = [];
 trumpObamacare = [];
@@ -17,22 +9,51 @@ clintonRussia = [];
 clintonGay = [];
 clintonKeystone =[];
 
-for s in statusesTrump:
-    if("rigged" in s.text.lower()):
-        trumpRigged.append(s)
-    else if("obamacare" in s.text.lower()):
-        trumpObamacare.append(s)
-    else if("gay" in s.text.lower()):
-        trumpGay.append(s)
-    else if("obama" in s.text.lower()):
-        trumpObama.append(s)
+def search():
+    auth = tweepy.OAuthHandler("xmE8yTuZ65lFReEbDUEIn5aqV", "z0tlMMPm7fxmIM5o21cbhfG5SS0NPgBLF6w4YxhyR0kCi4eeyE")
+    auth.set_access_token("797478447283077121-LsPU9sTIlxD7Mp54hYtoH1N9mR86rUV", "HAI9RdL4Mci6c2V5fAt4eK0wO3Geyat1Oq7KAxDINTKlu")
 
-for s in statusesClinton:
-    if("tpp" in s.text.lower()):
-        clintonTPP.append(s)
-    else if("russia" in s.text.lower()):
-        clintonRussia.append(s)
-    else if("keystone" in s.text.lower()):
-        clintonKeystone.append(s)
-    else if("gay" in s.text.lower()):
-        clintonGay.append(s)
+    api = tweepy.API(auth)
+
+    statusesTrump = tweepy.Cursor(api.user_timeline, user_id="25073877", count="10").items()
+    statusesClinton = tweepy.Cursor(api.user_timeline, user_id="1339835893", count="10").items();
+
+
+    for s in statusesTrump:
+        if("rigged" in s.text.lower()):
+            trumpRigged.append(s)
+        elif("obamacare" in s.text.lower()):
+            trumpObamacare.append(s)
+        elif("gay" in s.text.lower()):
+            trumpGay.append(s)
+        elif("obama" in s.text.lower()):
+            trumpObama.append(s)
+
+    for s in statusesClinton:
+        if("tpp" in s.text.lower()):
+            clintonTPP.append(s)
+        elif("russia" in s.text.lower()):
+            clintonRussia.append(s)
+        elif("keystone" in s.text.lower()):
+            clintonKeystone.append(s)
+        elif("gay" in s.text.lower()):
+            clintonGay.append(s)
+
+    return        
+
+def getClintonGay():
+    return clintonGay
+def getTrumpGay():
+    return trumpGay
+def getTrumpObama():
+    return trumpObama
+def getTrumpRigged():
+    return trumpRigged
+def getClintonGay():
+    return trumpObamacare
+def getClintonGay():
+    return clintonTPP
+def getClintonGay():
+    return clintonRussia
+def getClintonGay():
+    return clintonKeystone
